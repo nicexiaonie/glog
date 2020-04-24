cf3468f830442eb350ee6f9b130ab46f68002c41
聂元培 <nieyuanpei@qutoutiao.net>
2020-04-24 21:54:02 +0800

    添加自动删除文件

--- glog.go
+++ glog.go
@@ -1,8 +1,12 @@
 package glog
 
 import (
+	"fmt"
 	"github.com/nicexiaonie/glog/hook"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"time"
 )
 
 type Config struct {
@@ -14,8 +18,8 @@ type Config struct {
 	Output       string
 	ReportCaller bool
 	// 日志分割  "20060102150405"
-	Split string
-	//Lifetime time.Duration
+	Split    string
+	Lifetime time.Duration
 	//Rotation time.Duration
 }
 
@@ -52,6 +56,41 @@ func New(c *Config) *logrus.Logger {
 	default:
 		break
 	}
+
+	c.lifetime()
+
 	return log
 
+}
+
+func (current *Config) lifetime() {
+
+	if current.Lifetime == 0 {
+		return
+	}
+	fmt.Println(111)
+	go func(current *Config) {
+		for {
+			time.Sleep(current.Lifetime)
+			//fmt.Printf("sleep %s \n", current.Lifetime )
+			files, _ := ioutil.ReadDir(current.Path)
+			for _, file := range files {
+				if file.IsDir() {
+					continue
+				}
+				if current.Filename != file.Name()[0:len(current.Filename)] {
+					continue
+				}
+				d := time.Now().Sub(file.ModTime())
+				//fmt.Printf("sub %s \n", d )
+				if d > (current.Lifetime ) {
+					fileName := current.Path + file.Name()
+					//fmt.Printf("del %s \n", d )
+					_ = os.Remove(fileName)
+				}
+				//fmt.Println(current.Lifetime * time.Second)
+				//fmt.Printf("\n\n\n")
+			}
+		}
+	}(current)
 }
--- test/test.go
+++ test/test.go
@@ -1,21 +1,34 @@
 package main
 
-import "github.com/nicexiaonie/glog"
+import (
+	"fmt"
+	"github.com/nicexiaonie/glog"
+	"runtime"
+	"time"
+)
 
-func main()  {
-
+func main() {
 
 	logger := glog.New(&glog.Config{
-		Path: "/Users/nieyuanpei/Project/github/glog/log/",
-		Level: "debug",
-		Filename: "app.log",
-		Format: "json",
-		Output: "file",
+		Path:         "/Users/nieyuanpei/Project/github/glog/log/",
+		Level:        "debug",
+		Filename:     "app.log",
+		Format:       "json",
+		Output:       "file",
 		ReportCaller: true,
-		Split: "2006010215",
+		Split:        "2006010215",
+		Lifetime:     2 * time.Second,
 	})
-	logger.WithField("A",345).Errorf("adfet3tewtg")
+	logger.WithField("A", 345).Errorf("adfet3tewtg")
 
+	fmt.Println(runtime.NumGoroutine())
 
+	for {
+		time.Sleep(time.Second)
+		fmt.Println(runtime.NumGoroutine())
+	}
 
+	select {
+
+	}
 }
